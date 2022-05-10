29201424d774e045d049f6ab04a5aeaffada6788
Arend Moerman <arendmoerman@gmail.com>
2022-05-10 21:50:58 +0200

    Added optical system class

--- SystemInterface.py
+++ SystemInterface.py
@@ -2,39 +2,52 @@ import numpy as np
 import matplotlib.pyplot as pt
 
 import src.Python.Reflectors as Reflectors
+import src.Python.System as System
 
 def SystemInterface():
     
-    rot = np.radians([25, 70.33, 4])
-    cRot = np.array([25, 70.33, 4])
+    rot_p1 = np.radians([0, 0, 0])
+    cRot_p1 = np.array([0, 0, 0])
     
-    p = Reflectors.Parabola(b = 1000, cRot = cRot, offRot = rot)
-    h = Reflectors.Hyperbola()#offRot = rot)
-    e = Reflectors.Ellipse(b = 1000)
-    #print(help(e))
-    print(p)
-    print(h)
-    print(e)
-    limsa = 1
-    limsb = 10
+    rot_h1 = np.radians([0, 0, 0])
+    cRot_h1 = np.array([0, 0, 0])
+    offTrans_h1 = np.array([0, 0, 1000])
     
+    # Parabola p1 initialization
+    lims_x_p1 = 500
+    lims_y_p1 = 500
     
-    range_x = np.linspace(-limsa, limsa, 100)
-    range_y = np.linspace(-limsb, limsb, 100)
+    range_x_p1 = np.linspace(-lims_x_p1, lims_x_p1, 1000)
+    range_y_p1 = np.linspace(-lims_y_p1, lims_y_p1, 1000)
     
-    grid_x, grid_y = np.meshgrid(range_x, range_y)
+    grid_x_p1, grid_y_p1 = np.meshgrid(range_x_p1, range_y_p1)
     
-    p.setGrid(grid_x, grid_y)
-    p.rotateGrid()
-    p.plotReflector()
+    # Hyperbola h1 initialization
+    lims_x_h1 = 100
+    lims_y_h1 = 100
     
-    h.setGrid(grid_x, grid_y)
-    h.rotateGrid()
-    h.plotReflector()
+    range_x_h1 = np.linspace(-lims_x_h1, lims_x_h1, 100)
+    range_y_h1 = np.linspace(-lims_y_h1, lims_y_h1, 100)
     
-    e.setGrid(grid_x, grid_y)
-    e.rotateGrid()
-    e.plotReflector()
+    grid_x_h1, grid_y_h1 = np.meshgrid(range_x_h1, range_y_h1)
+    
+    ####
+    s = System.System()
+    s.addParabola(name = "p1", a = 50, b = 50, cRot = cRot_p1, offRot = rot_p1)
+    s.addHyperbola(name = "h1",a = 10, b = 10, c = 2, cRot = cRot_h1, offRot = rot_h1, offTrans = offTrans_h1)
+    
+    print(s.system["p1"])
+    print(s.system["h1"])
+    
+    s.system["p1"].setGrid(grid_x_p1, grid_y_p1)
+    s.system["p1"].rotateGrid()
+    s.system["p1"].plotReflector()
+    
+    s.system["h1"].setGrid(grid_x_h1, grid_y_h1)
+    s.system["h1"].rotateGrid()
+    s.system["h1"].plotReflector(color='red')
+    
+    s.plotSystem()
     
 if __name__ == "__main__":
     SystemInterface()

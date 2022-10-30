64f9827faad6a81a6bfbe2553e960842831b62f2
arend <arendmoerman@gmail.com>
2022-10-31 00:37:00 +0100

    Example interface added

--- run_example.py
+++ run_example.py
@@ -2,13 +2,15 @@ from examples.DRO_PSF_PO import ex_DRO_PO
 from examples.ASTE_PSF_PO import ex_ASTE_PO
 
 def select_example():
-    example = input("""Please select an example to run.\n
+    example = input("""Welcome to the POPPy example interface!
+Please select an example to run.
+
 Possible options: DRO_PO
                   ASTE_PO
 
--> """)
+> """)
 
-    print(example)
+    print("Running ex_{}.py\n".format(example))
     if example == "DRO_PO":
         ex_DRO_PO()
 

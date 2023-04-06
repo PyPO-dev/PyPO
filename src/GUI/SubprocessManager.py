cfadfb8494b21b0bed0c8bdb7582c6ca20b11c41
gafa <maikel.gafaji@gmail.com>
2023-04-06 20:41:01 +0200

    SubprocessManager in progress

--- src/GUI/SubprocessManager.py
+++ src/GUI/SubprocessManager.py
@@ -1,56 +1,74 @@
-from multiprocessing import Process
+from multiprocessing import Process, Manager
 from PyQt5.QtCore import QThread
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QWidget, QMainWindow, QApplication, QVBoxLayout, QPushButton, QLabel
 from Dialogs import SymDialog
 from Waiter import Waiter
 from time import sleep
 
 class SubprocessManager():
-    def __init__(self) -> None:
+    def __init__(self, parentWidget) -> None:
+        self.parentWidget = parentWidget
         self.subProcessRunning = False
 
 
-    def waiterFinished(self , x):
-        print(f"Waiter finished {x = }")
+    def waiterFinished(self , success):
+        if success:
+            self.currentCalculationDialog.accept()
+        else:
+            self.currentCalculationDialog.reject()
+        self.subProcessRunning = False
 
-    def runInSubprocess(self, work, args, propBeamDict):
+
+    def runInSubprocess(self, work, args, propBeamDict, dialogText = ""):
         if self.subProcessRunning:
             return
         self.subProcessRunning = True
 
         process = Process(target = work, args = args)
 
-        dialStr = f"Calculating"# {subStr} on {propBeamDict['t_name']}..."
-        self.currentCalculationDialog = SymDialog(process.kill, None, dialStr) 
+        if not dialogText:
+            dialogText = "Calculating"
+        self.currentCalculationDialog = SymDialog(process.kill, None, dialogText) 
         self.currentCalculationDict = propBeamDict
 
-        def calculationFinished(exitCode):
-            waiter.deleteLater()
-            self.waiterThread.deleteLater()
-            self.waiterFinished(exitCode==0)
         
-        self.waiterThread = QThread(parent=self)
+        self.waiterThread = QThread(parent=self.parentWidget)
         waiter = Waiter()
         waiter.setProcess(process)
         waiter.moveToThread(self.waiterThread)
         self.waiterThread.started.connect(waiter.run)
-        waiter.finished.connect(calculationFinished)
+        waiter.finished.connect(self.waiterFinished)
 
         process.start()
         self.waiterThread.start()
         self.currentCalculationDialog.exec_()
 
 
-def func(x):
-    for i in range(x):
-        sleep(1)
-        print(f"working {i = }")
+
 
 if __name__ == "__main__":
     import sys
+    class window(QWidget):
+        def __init__(self) -> None:
+            super().__init__()
+            layout = QVBoxLayout(self)
+            btn = QPushButton("run subprocess")
+            btn.clicked.connect(self.runSubprocess)
+            layout.addWidget(btn)
+
+
+
+            self.subProc = SubprocessManager(self)
+
+        def runSubprocess(self):
+            self.subProc.runInSubprocess(self.func, (5,), None)
+
+        def func(self, x):
+            for i in range(x):
+                sleep(1)
+                print(f"counting {i = }")
 
     app = QApplication(sys.argv)
-    test = SubprocessManager()
+    test = window()
     test.show()
-    test.runInSubprocess(func, (5,), {1:1})
     sys.exit(app.exec_())

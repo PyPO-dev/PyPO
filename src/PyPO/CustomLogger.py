817ce6aa59134c282be9ed7430891fa52d3063c3
gafa <maikel.gafaji@gmail.com>
2023-05-07 12:43:21 +0200

    ensureCursorVisible

--- src/PyPO/CustomLogger.py
+++ src/PyPO/CustomLogger.py
@@ -90,6 +90,7 @@ class GUILogger(logging.Handler):
 
     def emit(self, record):
         self.edit.append(self.format(record))
+        self.edit.ensureCursorVisible()
 
 ##
 # Class for instantiating a GUI logger.

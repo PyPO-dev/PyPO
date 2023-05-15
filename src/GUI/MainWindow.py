aa2254b5e6511d81283a25418759266908fbfdde
gafa <maikel.gafaji@gmail.com>
2023-05-16 00:19:06 +0200

    bug fix

--- src/GUI/MainWindow.py
+++ src/GUI/MainWindow.py
@@ -829,7 +829,7 @@ class MainWidget(QWidget):
     def addPropRaysAction(self): 
         try:
             propRaysDict = self.ParameterWid.read()
-            chk.check_runRTDict(propRaysDict, self.stm, self.stm.frames, self.clog)
+            chk.check_runRTDict(propRaysDict, self.stm.system, self.stm.frames, self.clog)
         
             self.stm.runRayTracer(propRaysDict)
             self.addFrameWidget(propRaysDict["fr_out"])

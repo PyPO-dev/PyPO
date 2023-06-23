322e60ec25710137bd0e23b4ee18338641ac834d
arend <arendmoerman@gmail.com>
2023-06-23 09:27:08 +0200

    Save as and load now open up on last path

--- docs/search/all_b.js
+++ docs/search/all_b.js
@@ -1,14 +1,13 @@
 var searchData=
 [
-  ['lam_0',['lam',['../structScalarGPODict.html#a957209e5bcba36eda01fd6d61d040473',1,'ScalarGPODict::lam()'],['../structGPODictf.html#a34bda8ca611e8ebef80d8c5e364b7c7a',1,'GPODictf::lam()'],['../structScalarGPODictf.html#a04984941dc0fa0365fe314730ace0905',1,'ScalarGPODictf::lam()'],['../structGPODict.html#a09bafcf782b5bfd3b7e4cf1957b48b2c',1,'GPODict::lam()']]],
+  ['lam_0',['lam',['../structGPODict.html#a09bafcf782b5bfd3b7e4cf1957b48b2c',1,'GPODict::lam()'],['../structScalarGPODict.html#a957209e5bcba36eda01fd6d61d040473',1,'ScalarGPODict::lam()'],['../structGPODictf.html#a34bda8ca611e8ebef80d8c5e364b7c7a',1,'GPODictf::lam()'],['../structScalarGPODictf.html#a04984941dc0fa0365fe314730ace0905',1,'ScalarGPODictf::lam()']]],
   ['loadbeamlib_1',['loadBeamlib',['../BindBeam_8py.html#ae5fb76d06da0017b1261789f413c8c84',1,'PyPO::BindBeam']]],
   ['loadcpulib_2',['loadCPUlib',['../BindCPU_8py.html#a0a81f4a38dc6621d9079039afa39e65b',1,'PyPO::BindCPU']]],
   ['loadgpulib_3',['loadGPUlib',['../BindGPU_8py.html#a6a416349c18f3ea14dc299c90e8ad936',1,'PyPO::BindGPU']]],
   ['loadrefllib_4',['loadRefllib',['../BindRefl_8py.html#a95276632770e1616fac5b52f55fe2cd9',1,'PyPO::BindRefl']]],
-  ['loadsystem_5',['loadSystem',['../group__public__api__sysio.html#gae622c9e52028247509a83e282c27cdbd',1,'PyPO::System::System']]],
-  ['loadsystemaction_6',['loadSystemAction',['../classGUI_1_1MainWindow_1_1MainWidget.html#a9d33910b9043d3faac5a64b304892592',1,'GUI::MainWindow::MainWidget']]],
-  ['loadsystemform_7',['loadSystemForm',['../classGUI_1_1MainWindow_1_1MainWidget.html#a2365928fb9f3a0af8c2aeee7db6d0eeb',1,'GUI.MainWindow.MainWidget.loadSystemForm()'],['../formData_8py.html#a337197d35108c69f2199211bf19de8a5',1,'GUI.ParameterForms.formData.loadSystemForm()']]],
-  ['loadtransflib_8',['loadTransflib',['../BindTransf_8py.html#ad8d0b7ca5fb5b5a7892f88fd3a94a99b',1,'PyPO::BindTransf']]],
-  ['lxu_9',['lxu',['../structreflparamsf.html#a55a1df398503cc1e9fcd63e49bbff8ee',1,'reflparamsf::lxu()'],['../structreflparams.html#a748214eefdc845e7ec75fe5bc7adff81',1,'reflparams::lxu()']]],
-  ['lyv_10',['lyv',['../structreflparamsf.html#ab4b1a1219effaf7763896b602d0604a0',1,'reflparamsf::lyv()'],['../structreflparams.html#ad637efde9527b76c2063e91466451ab1',1,'reflparams::lyv()']]]
+  ['loadsystem_5',['loadSystem',['../group__public__api__sysio.html#gae622c9e52028247509a83e282c27cdbd',1,'PyPO.System.System.loadSystem()'],['../classGUI_1_1MainWindow_1_1MainWidget.html#ab6001f21cb0213453faf7466f2bb2d68',1,'GUI.MainWindow.MainWidget.loadSystem()']]],
+  ['loadsystemform_6',['loadSystemForm',['../formData_8py.html#a337197d35108c69f2199211bf19de8a5',1,'GUI::ParameterForms::formData']]],
+  ['loadtransflib_7',['loadTransflib',['../BindTransf_8py.html#ad8d0b7ca5fb5b5a7892f88fd3a94a99b',1,'PyPO::BindTransf']]],
+  ['lxu_8',['lxu',['../structreflparams.html#a748214eefdc845e7ec75fe5bc7adff81',1,'reflparams::lxu()'],['../structreflparamsf.html#a55a1df398503cc1e9fcd63e49bbff8ee',1,'reflparamsf::lxu()']]],
+  ['lyv_9',['lyv',['../structreflparams.html#ad637efde9527b76c2063e91466451ab1',1,'reflparams::lyv()'],['../structreflparamsf.html#ab4b1a1219effaf7763896b602d0604a0',1,'reflparamsf::lyv()']]]
 ];
